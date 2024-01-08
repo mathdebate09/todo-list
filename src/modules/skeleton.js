2a798a7b057ede64e72e316655ba351770558fa5
Jay Singh <learn.jayssingh@gmail.com>
2024-01-09 00:06:41 +0530

    HTML Skeleton rendered with Js

---
+++ src/modules/skeleton.js
@@ -0,0 +1,78 @@
+import logoPath from './assets/logo.svg';
+import homePath from './assets/home.svg';
+import inboxPath from './assets/inbox.svg';
+import plusPath from './assets/plus.svg';
+import projectPath from './assets/project.svg';
+import todayPath from './assets/today.svg';
+import weekPath from './assets/week.svg';
+
+/* Creates
+        <div class="header">
+            <img class="logo-img" src="">
+            <p class="logo-text">PixelDo</p>
+        </div>
+        <div class="display">
+            <div class="tools">
+                <div class="todos">
+                    <button class="inbox"><img src=""> Tasks</button>
+                    <button class="today"><img src=""> Today</button>
+                    <button class="this-week"><img src=""> This Week</button>
+                </div>
+                <div class="projects">
+                    <p class="project-title">Projects</p>
+                    <button class="add-project"><img src=""> Add Project</button>
+                </div>
+            </div>
+            <div class="action-display">
+                <p class="action-header"><img src="">Tasks</p>
+                <button class="add-task"><img src="">Add Task</button>
+            </div>
+        </div> */
+
+const container = document.querySelector('.container');
+
+const headerDiv = document.createElement('div');
+headerDiv.classList.add('header');
+headerDiv.innerHTML = `
+    <img class="logo-img" src="${logoPath}">
+    <p class="logo-text">PixelDo</p>`;
+container.appendChild(headerDiv);
+
+const displayDiv = document.createElement('div');
+displayDiv.classList.add('display');
+
+const toolsDiv = document.createElement('div');
+toolsDiv.classList.add('tools');
+
+const todosDiv = document.createElement('div');
+todosDiv.classList.add('todos');
+todosDiv.innerHTML = `
+    <button class="inbox"><img src="${inboxPath}">Tasks</button>
+    <button class="today"><img src="${todayPath}">Today</button>
+    <button class="this-week"><img src="${weekPath}">This Week</button>`;
+toolsDiv.appendChild(todosDiv);
+
+const projectsDiv = document.createElement('div');
+projectsDiv.classList.add('projects');
+projectsDiv.innerHTML = `
+    <p class="project-title">Projects</p>
+    <div class="project-list"></div>
+    <button class="add-project"><img src="${projectPath}">Add Project</button>`;
+toolsDiv.appendChild(projectsDiv);
+
+displayDiv.appendChild(toolsDiv);
+
+const actionDisplayDiv = document.createElement('div');
+actionDisplayDiv.classList.add('action-display');
+actionDisplayDiv.innerHTML = `
+    <p class="action-header"><img src="${homePath}">Tasks</p>
+    <div class="task-list"></div>
+    <button class="add-task"><img src="${plusPath}">Add Task</button>`;
+displayDiv.appendChild(actionDisplayDiv);
+
+container.appendChild(displayDiv);
+
+const createFooter = document.createElement('div');
+createFooter.classList.add('footer');
+createFooter.innerHTML = '<p>© Jay Singh <a href="https://github.com/mathdebate09/todo-list" target="_blank">(mathdebate09)</a></p>'
+container.appendChild(createFooter);
